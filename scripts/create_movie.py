1c31e54a8b2b5cebff88fc7ac9b333ef06e588c6
Mahsa R <mahsarm99@gmail.com>
2023-12-26 11:18:08 +0330

    add script file for creating movie objects by admins

---
+++ scripts/create_movie.py
@@ -0,0 +1,19 @@
+from argparse import ArgumentParser
+from movie import Movie
+
+
+def main():
+    parser = ArgumentParser(
+        description="This script is for creating Movie object by admins."
+    )
+    parser.add_argument("--name", type=str, help="Movie Name", required=True)
+    parser.add_argument("--genere", type=str, help="Movie genere", required=True)
+    parser.add_argument("--age_limit", type=int, help="Movie age limit", required=True)
+    parser.add_argument("--price", type=float, help="Movie price", required=True)
+
+    args = parser.parse_args()
+    Movie(args.name, args.genere, args.age_limit, args.price)
+
+
+if __name__ == "__main__":
+    main()
