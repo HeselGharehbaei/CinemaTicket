eda688f81848e6aa3dc328d2c27a89a956f84b30
hesel <hgharehbaai@gmail.com>
2023-12-30 22:16:43 +0330

    perf: Add main function and conditional execution for module-specific actions

--- scripts/managers.py
+++ scripts/managers.py
@@ -11,12 +11,17 @@ sys.path.append(parent_dir)
 from user import User, UserType
 
 
-parser= ArgumentParser(description= "Manager User")
-parser.add_argument("username", metavar="USERNAME", type=str, help='your username not be doplicated')
-parser.add_argument("password", metavar="PASSWORD", type=str, help='your password with 4 or upper than 4 character')
-parser.add_argument("birthday", metavar="BIRTHDAY", type=str, help='your birthday in format of dd/mm/yyyy')
-parser.add_argument("-ph", "--phonenumber", metavar="PHONE NUMBER", type=str, help='your phone number')
-parser.add_argument("-ut", "--user_type", metavar="USER TYPE", type= object, help='your user type that manager or normal user')
-args = parser.parse_args()
+def main():
+    parser= ArgumentParser(description= "Manager User")
+    parser.add_argument("username", metavar="USERNAME", type=str, help='your username not be doplicated')
+    parser.add_argument("password", metavar="PASSWORD", type=str, help='your password with 4 or upper than 4 character')
+    parser.add_argument("birthday", metavar="BIRTHDAY", type=str, help='your birthday in format of dd/mm/yyyy')
+    parser.add_argument("-ph", "--phonenumber", metavar="PHONE NUMBER", type=str, help='your phone number')
+    parser.add_argument("-ut", "--user_type", metavar="USER TYPE", type= object, help='your user type that manager or normal user')
+    args = parser.parse_args()
 
-manager= User(args.username, args.password, args.birthday, args.phonenumber, user_type= UserType.MANAGER)
+    manager= User(args.username, args.password, args.birthday, args.phonenumber, user_type= UserType.MANAGER)
+
+
+if __name__ == "__main__":
+    main()    
