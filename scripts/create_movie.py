f487690954875ce17aa7f2e8b51d8272627ef258
hesel <hgharehbaai@gmail.com>
2024-03-13 03:23:03 +0330

    fix: modify directory path to run the script

--- scripts/create_movie.py
+++ scripts/create_movie.py
@@ -1,3 +1,9 @@
+import os, sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
+sys.path.append(parent_dir)
+
+
 from argparse import ArgumentParser
 from movie import Movie
 
